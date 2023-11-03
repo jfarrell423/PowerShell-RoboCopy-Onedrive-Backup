728fbb28966fea97067d1ea999edfc7941a3321f
Jerry D Farrell <14806267+jfarrell423@users.noreply.github.com>
2023-11-03 17:25:44 -0400

    Adding the PowerShell Script to Run RoboCopy

---
+++ OneDrive.ps1
@@ -0,0 +1,10 @@
+﻿# Powershell Script to Backup my Tennessee OneDrive
+# This Script needs to run on NTFS File Systems
+# Where there are files larger than 4gb
+# Else you will generate an error 87
+
+$source = "C:\Users\username\OneDrive"
+$destination = "J:\OneDrive"
+
+# Copy files from source to destination, only copying newer or changed files and ignoring deletions
+robocopy $source $destination /E /COPY:DAT /DCOPY:T /PURGE /XO
